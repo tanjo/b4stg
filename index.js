d95e802dc71d5c7227125cba68a5a925c8c9dd70
tanjo <tanjo@users.noreply.github.com>
2019-12-18 16:38:32 +0900

    generate tepalte html

---
+++ index.js
@@ -0,0 +1,41 @@
+#!/usr/bin/env node
+
+const fs = require('fs');
+const path = require('path');
+const commander = require('commander');
+const packageJson = require('./package.json');
+
+const main = () => {
+  commander
+      .version(packageJson.version, '-v, --version')
+      .parse(process.argv);
+
+   const argc = commander.args.length;
+
+  let destination;
+  if (argc < 1) {
+    destination = process.cwd() + "/index.html"
+  } else {
+    destination = path.resolve(commander.args[0]);
+  }
+
+  if (fs.existsSync(destination)) {
+    console.error(`[Error]: File exists ${destination}`);
+    return;
+  }
+
+  let basename = path.basename(destination);
+  let extname = path.extname(destination);
+  try {
+    switch (true) {
+      case /html/i.test(extname):
+        fs.copyFileSync(path.resolve("template/HTML"), destination, fs.constants.COPYFILE_EXCL);
+        console.log(`[Info]: Generate ${destination}`)
+        return;
+    }
+  } catch (err) {
+    console.error(`[Error]: ${err}`);
+  }
+};
+
+main();
